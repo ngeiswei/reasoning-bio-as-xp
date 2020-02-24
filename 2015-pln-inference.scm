1fb45d0d3ad3d8ba0daccfb6055635db4f6d6c61
Nil Geisweiller <ngeiswei@gmail.com>
2020-02-24 14:26:18 +0200

    Experiment with intensional reasoning

--- 2015-pln-inference.scm
+++ 2015-pln-inference.scm
@@ -3,9 +3,9 @@
 
 ;; Parameters
 (define rs 0)                           ; Random seed
-(define ss 1)                         ; Subsampled portion of the KBs
-(define mi 1000)                      ; Maximum number of iterations
-(define cp 10)                           ; Complexity penalty
+(define ss 0.001)                         ; Subsampled portion of the KBs
+(define mi 100)                      ; Maximum number of iterations
+(define cp 1)                           ; Complexity penalty
 
 ;; Load modules
 (use-modules (opencog randgen))
@@ -15,12 +15,15 @@
 (use-modules (opencog bioscience))
 (load "bio-as-utils.scm")
 
+;; Parameters string
+(define param-str (string-append
+                   "-rs=" (number->string rs)
+                   "-ss=" (number->string ss)
+                   "-mi=" (number->string mi)
+                   "-cp=" (number->string cp)))
+
 (define log-filename
-  (string-append "2015-pln-xp"
-                 "-rs=" (number->string rs)
-                 "-ss=" (number->string ss)
-                 "-cp=" (number->string cp)
-                 ".log"))
+  (string-append "log/2015-pln-xp" param-str ".log"))
 
 ;; (cog-logger-set-timestamp! #f)
 ;; (cog-logger-set-sync! #t)
@@ -32,34 +35,56 @@
 (ure-logger-set-filename! log-filename)
 
 ;; Load kbs
-(define db-lst (load-kbs (list "kbs/GO.scm"
-                               "kbs/GO_annotation.scm"
-                               "kbs/2015-completion.scm")
+(define db-lst (load-kbs (list
+                          ;; Obtained from running preprocess-kbs.scm
+                          "results/preprocess-kbs-rs=0-ss=1-mi=12-cp=10-fra=t.scm")
                          #:subsmp ss))
+
+(cog-logger-debug "(length db-lst) = ~a" (length db-lst))
 
 ;; Load PLN
 (pln-load #:rule-base 'empty)
+(pln-add-rule-by-name "subset-condition-negation-rule")
+(pln-add-rule-by-name "subset-attraction-introduction-rule")
+(pln-add-rule-by-name "intensional-inheritance-direct-introduction-rule")
+(pln-add-rule-by-name "intensional-similarity-direct-introduction-rule")
+
+;; For now calculate the intensional inheritance between GO concepts
+;; as a test.
+
+;; Run backward chainer to produce attraction links.
+(define X (Variable "$X"))
+(define Y (Variable "$Y"))
+(define CT (Type "ConceptNode"))
+(define vardecl (VariableSet
+                  (TypedVariable X CT)
+                  (TypedVariable Y CT)))
+(define target (Attraction X Y))
+(define results-ats (pln-bc target
+                            #:vardecl vardecl
+                            #:maximum-iterations mi
+                            #:complexity-penalty cp))
+
+;; Run backward chainer to produce intensional inheritances.
+;; (define target (IntensionalInheritance X Y))
+(define target (IntensionalSimilarity X Y))
+(define results-iis (pln-bc target
+                            #:vardecl vardecl
+                            #:maximum-iterations mi
+                            #:complexity-penalty cp))
 
 ;; ;; Run forward chainer
-;; (define vardecl (VariableSet (TypedVariable X ConceptT) (TypedVariable Y ConceptT)))
-;; (define source (Inheritance X Y))
+;; (define vardecl (VariableSet (TypedVariable X ConceptT)))
+;; (define source X)
 ;; (define results (pln-fc source
 ;;                         #:vardecl vardecl
 ;;                         #:maximum-iterations mi
 ;;                         #:complexity-penalty cp))
 
-;; For now apply the following rules in a determined order
-
-
-
-;; Conclusion
-
+;; All results
+(define results (append (cog-outgoing-set results-ats)
+                        (cog-outgoing-set results-iis)))
 
 ;; Write results in file
-(define scm-filename
-  (string-append "2015-pln-xp"
-                 "-rs=" (number->string rs)
-                 "-ss=" (number->string ss)
-                 "-cp=" (number->string cp)
-                 ".scm"))
-(write-atoms-to-file scm-filename (cog-outgoing-set results))
+(define scm-filename (string-append "results/2015-pln-xp" param-str ".scm"))
+(write-atoms-to-file scm-filename results)
