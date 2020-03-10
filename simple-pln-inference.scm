c661928465d45dd7eb503403aa6866a4a410ea2f
Nil Geisweiller <ngeiswei@gmail.com>
2020-03-10 10:23:01 +0200

    Fix simple PLN inference

--- simple-pln-inference.scm
+++ simple-pln-inference.scm
@@ -10,7 +10,7 @@
 ;; Parameters
 (define rs 0)                           ; Random seed
 (define ss 1)                         ; Subsampled portion of the KBs
-(define mi 10000000)                      ; Maximum number of iterations
+(define mi 100000)                      ; Maximum number of iterations
 (define cp 1)                           ; Complexity penalty
 
 ;; Load modules
@@ -40,38 +40,74 @@
 (ure-logger-set-level! "debug")
 (ure-logger-set-filename! log-filename)
 
-;; Load kbs
-(define db-lst (load-kbs (list
-                          ;; Obtained from running preprocess-kbs.scm
-                          "results/preprocess-kbs-aging-rs=0-ss=1-mi=1000-cp=1.scm")
-                         #:subsmp ss))
+;; Use GO namespace as extra pattern for intensional reasoning
+;;
+;; New interaction evaluation base on intensiona similarity
 
-;; (cog-logger-debug "db-lst = ~a" db-lst)
+;; Load kb restricted to aging
+(define aging-db (load-kbs (list
+                            ;; Obtained from running preprocess-kbs.scm
+                            "results/preprocess-kbs-aging-rs=0-ss=1-mi=1000-cp=1.scm")))
+;; (cog-logger-debug "aging-db = ~a" aging-db)
 
-;; Load PLN
+;; Load empty PLN
 (pln-load #:rule-base 'empty)
-(pln-add-rule-by-name "intensional-similarity-direct-introduction-rule")
 
-;; Add extra specialized rules
+;; Definitions
+(define X (Variable "$X"))
+(define GT (Type 'GeneNode))
+(define CT (Type 'ConceptNode))
+
+;; 1. Infer all the genes that relate to aging
+(pln-load-from-path "rules/transfer-relates-to-aging-to-member.scm")
+(pln-add-rule-by-name "transfer-relates-to-aging-to-member-rule")
+(define source (Evaluation (Predicate "relates-to-aging") X))
+(define vardecl (TypedVariable X CT))
+(define gra-results (pln-fc source
+                            #:vardecl vardecl
+                            #:maximum-iterations mi
+                            #:complexity-penalty cp))
+(cog-logger-debug "gra-results = ~a" gra-results)
+
+;; Write results in file (so we don't need to recompute it next time
+(define scm-filename (string-append "results/genes-related-to-aging" param-str ".scm"))
+(write-atoms-to-file scm-filename (cog-outgoing-set gra-results))
+
+;; 2. Infer that if some gene relates to aging and is similar to
+;;    another gene, then that one relates to aging too.
+
+;; Load extra rules
+(pln-add-rule-by-name "intensional-similarity-direct-introduction-rule")
 (pln-load-from-path "rules/similar-genes-relating-to-aging.scm")
 (pln-add-rule-by-name "similar-genes-relating-to-aging-rule")
 (pln-load-from-path "rules/transfer-intensional-similarity-to-member.scm")
 (pln-add-rule-by-name "transfer-intensional-similarity-to-member-rule")
-(pln-load-from-path "rules/transfer-relates-to-aging-to-member.scm")
-(pln-add-rule-by-name "transfer-relates-to-aging-to-member-rule")
 
-;; Define sources and run forward chainer on them
-(define X (Variable "$X"))
-(define Y (Variable "$Y"))
-(define X-vardecl (TypedVariable X (Type 'ConceptNode)))
-(define Y-vardecl (TypedVariable Y (Type 'ConceptNode)))
-(define vardecl (VariableSet X-vardecl Y-vardecl))
-(define sources X)
-(define results (pln-fc sources
-                        #:vardecl X-vardecl
-                        #:maximum-iterations mi
-                        #:complexity-penalty cp))
+;; Load full kb in order to infer new genes related to aging
+(define full-db (load-kbs (list
+                           "results/preprocess-kbs-rs=0-ss=1-mi=12-cp=10-fra=t.scm")
+                          #:subsmp ss))
+
+;; Fetch all GO categories related to aging, with strength above zero,
+;; to use them as sources
+(define relates-to-aging-facts (load-kb "facts/relates-to-aging.scm"))
+(define go-categories-related-to-aging (filter (lambda (x) (not (null-mean? x)))
+                                               (map gdr relates-to-aging-facts)))
+;; (cog-logger-debug "go-categories-related-to-aging = ~a" go-categories-related-to-aging)
+(define trace-as (cog-new-atomspace))
+(define sources (Set go-categories-related-to-aging))
+(define final-results (pln-fc sources
+                              #:maximum-iterations mi
+                              #:complexity-penalty cp
+                              #:trace-as trace-as))
+
+(cog-logger-debug "final-results = ~a" final-results)
 
 ;; Write results in file
-(define scm-filename (string-append "results/simple-pln-inference" param-str ".scm"))
-(write-atoms-to-file scm-filename (cog-outgoing-set results))
+(define scm-fn (string-append "results/simple-pln-inference" param-str ".scm"))
+(write-atoms-to-file scm-filename (cog-outgoing-set final-results))
+
+;; Write trace in file
+(cog-set-atomspace! trace-as)
+(define scm-trace-fn (string-append "results/simple-pln-inference-trace" param-str ".scm"))
+(write-atoms-to-file scm-trace-fn (cog-get-all-roots))
